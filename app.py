6e196057f304667394ec946a16ef5b5f32445d85
23f3001930 <23f3001930@ds.iitm.study.ac.in>
2025-06-14 01:03:50 +0530

    hj

--- app.py
+++ app.py
@@ -727,7 +727,7 @@ async def health_check():
         )
 @app.get("/")
 async def root():
-    return {"message": "🎉 FastAPI app is deployed on Vercel and running successfully!"}
+    return {"message": " FastAPI app is deployed on Vercel and running successfully!"}
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
